d84b1a8c0c1e02909f2b06dee1297a12619cb038
sourav415 <souravnandi980@gmail.com>
2022-02-05 09:09:40 +0530

    terraform data

---
+++ main.tf
@@ -0,0 +1,32 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.91.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+# create data factory
+
+resource "azurerm_data_factory" "example" {
+
+  name = "my-adf1"
+  resource_group_name = azurerm_resource_group.example.name
+  location = azurerm_resource_group.example.location
+
+  
+}
